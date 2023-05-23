aec745f953e8e2cdca2fa46173e2383b9840a547
nithinRaj031197 <nithinraj.official3@gmail.com>
2023-05-24 01:55:19 +0530

    create todo setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div>page</div>;
+import { prisma } from "@/db";
+import Link from "next/link";
+import TodoItem from "./components/TodoItem";
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+  // await prisma.todo.create({ data: { title: "test", description: "ok" } });
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 outline-none focus-within:bg-slate-700"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
+  );
 }
